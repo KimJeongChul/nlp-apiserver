6ae38b58b5308912f3197e4611e9029d0fdf0119
KimJeongChul <kjc5443@gmail.com>
2022-03-07 01:06:41 +0900

    [UPDATE] adding init, get localMacAddr

--- api/server.go
+++ api/server.go
@@ -5,8 +5,13 @@ import (
 	"nlp-apiserver/config"
 	"nlp-apiserver/errors"
 	"nlp-apiserver/logger"
+	"nlp-apiserver/utils"
 
 	"github.com/gorilla/mux"
+)
+
+const (
+	packageName = "api"
 )
 
 // ApiServer
@@ -15,29 +20,44 @@ type ApiServer struct {
 	router       *mux.Router
 }
 
+var uuidCnt int
+var localMacAddr string
+
+func init() {
+	uuidCnt = 0
+	localMacAddrs, errMac := utils.GetMacAddress()
+	if errMac != nil {
+		logger.LogE(packageName, "init", "utils.GetMacAddress Err Msg=", errMac.Error())
+		panic(errMac)
+	}
+	localMacAddr = localMacAddrs[0]
+}
+
 // NewApiServer Create new ApiServer
 func NewApiServer(configJson *config.ServerConfigJson) *ApiServer {
 	var as ApiServer
 	as.serverConfig = configJson
 
 	as.router = mux.NewRouter()
+	as.router.HandleFunc("/train", as.handleTrainCreate)
 
 	return &as
 }
 
 // Listen Create http, https server and ListenAndServe
 func (as *ApiServer) Listen() int {
+	funcName := "Listen"
 	var err error
 	if as.serverConfig.Ssl == 0 {
-		logger.LogI("Listen", "api", "HTTP Listening:Port=", as.serverConfig.ListenPort)
+		logger.LogI(packageName, funcName, "HTTP Listening:Port=", as.serverConfig.ListenPort)
 		err = http.ListenAndServe(":"+as.serverConfig.ListenPort, as.router)
 		cErr := errors.NewCError(errors.HTTP_SERVE_ERR, err.Error())
-		logger.LogE("Listen", "api", "ERR:Msg=", cErr.Error())
+		logger.LogE(packageName, funcName, "ERR:Msg=", cErr.Error())
 	} else {
-		logger.LogI("Listen", "api", "HTTPs Listening:Cert=", as.serverConfig.CertPemPath, ",keyPem=", as.serverConfig.KeyPemPath)
+		logger.LogI(packageName, funcName, "HTTPs Listening:Cert=", as.serverConfig.CertPemPath, ",keyPem=", as.serverConfig.KeyPemPath)
 		err = http.ListenAndServeTLS(":"+as.serverConfig.ListenPort, as.serverConfig.CertPemPath, as.serverConfig.KeyPemPath, as.router)
 		cErr := errors.NewCError(errors.HTTPS_SERVE_ERR, err.Error())
-		logger.LogE("Listen", "api", "ERR:Msg=", cErr.Error())
+		logger.LogE(packageName, funcName, "ERR:Msg=", cErr.Error())
 	}
 	if err != nil {
 		panic(err)
