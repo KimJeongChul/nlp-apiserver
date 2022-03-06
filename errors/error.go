ec3e5db134539e2b47293bb895a14eb0645084f7
KimJeongChul <kjc5443@gmail.com>
2022-03-07 00:46:37 +0900

    [ADD] errors

---
+++ errors/error.go
@@ -0,0 +1,31 @@
+package errors
+
+import "strconv"
+
+type CError struct {
+	Code    ErrCode
+	Message string
+}
+
+func NewCError(code ErrCode, msg string) *CError {
+	return &CError{
+		Code:    code,
+		Message: msg,
+	}
+}
+
+func (e *CError) Error() string {
+	return "CODE:" + strconv.Itoa(int(e.Code)) + " MSG:" + e.Message
+}
+
+type ErrCode int
+
+const (
+	SUCCESS ErrCode = iota
+
+	// HTTP_SERVER_ERR http.ListenAndServe() error
+	HTTP_SERVE_ERR
+
+	// HTTPS_SERVE_ERR https.ListenAndServeTLS() error
+	HTTPS_SERVE_ERR
+)
