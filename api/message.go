859e1c40364436efc3a83541c70f7cdb5154eac3
KimJeongChul <kjc5443@gmail.com>
2022-03-07 12:22:35 +0900

    [ADD] message.go

---
+++ api/message.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"nlp-apiserver/errors"
+)
+
+type MsgCommonRes struct {
+	ResultCode int    `json:"resultCode"`
+	Result     string `json:"result"`
+}
+
+func (as *ApiServer) responseErrorMessage(w http.ResponseWriter, cErr *errors.CError, errCode int) {
+	msgCommonRes := MsgCommonRes{
+		ResultCode: errCode,
+		Result:     cErr.Message,
+	}
+	wByte, err :=  json.Marshal(msgCommonRes)
+	if err != nil {
+		cErr, errCode = errors.NewCError(errors.JSON_MARSHAL_ERR, err.Error()), http.StatusInternalServerError
+		message := "{\"resultCode\":500, \"result\":\""+cErr.Error()+"\"}"
+		w.Write([]byte(message))
+	}
+	w.Write(wByte)
+}
