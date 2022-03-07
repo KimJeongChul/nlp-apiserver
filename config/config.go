ef9f469b053e76f54e22c841e83f5398b3304220
KimJeongChul <kjc5443@gmail.com>
2022-03-07 13:27:25 +0900

    [UPDATE] adding homePath

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 // ServerConfigJson Server config
 type ServerConfigJson struct {
 	ListenPort  string `json:"listenPort"`
+	HomePath    string `json:"homePath"`
 	LogPath     string `json:"logPath"`
 	LogLevel    string `json:"LogLevel"`
 	Ssl         int    `json:"ssl"`
